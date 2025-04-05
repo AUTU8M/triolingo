98c6e9e1e3a591fe818446baa701f9ac695254e1
sayandeepmondal1013 <sayandeepmondal1013@gmail.com>
2025-04-05 06:21:12 +0000

    Update widget test to match DuolingoSplashScreen UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:triolingo/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Check DuolingoSplashScreen elements', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the "duolingo" text is present.
+    expect(find.text('duolingo'), findsOneWidget);
+    // Verify that the "GET STARTED" button is present.
+    expect(find.widgetWithText(ElevatedButton, 'GET STARTED'), findsOneWidget);
   });
 }
